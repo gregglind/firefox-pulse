d0fce9a221e9cf26b2b0420ab2d14a89523bb656
Gregg Lind <gregg.lind@gmail.com>
2014-08-14 14:21:04 -0500

    Fix #25.  Panel exists, but rough.  Fires on first open, and by kbd

---
+++ data/ui/doorhanger.js
@@ -0,0 +1,52 @@
+/*! This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+"use strict";
+
+
+self.port.on("options", function (options){
+	document.getElementById("textbox").innerHTML = options.message;
+	document.getElementById("header").innerHTML = options.header;
+	document.getElementById("button").innerHTML = options.buttonLabel;
+	
+	if (!options.explanationMessage) options.explanationMessage = "";
+	document.getElementById("explanationsection").innerHTML = options.explanationHeader + " " + options.explanationMessage;
+
+	//setting the callback
+	document.getElementById("button").addEventListener("click", buttonClick);
+
+	/*if (options.icon) {   // #134 decided against this.
+		document.getElementById("icon").src = options.icon;
+	}*/
+	changeBodySize(options.panelSize);
+
+	try {
+		if (options.arm.explanation == "unexplained" || options.explanationHide){
+			document.getElementById("explanationsection").style.visibility = "hidden";
+		}
+		else{
+			document.getElementById("explanationsection").style.visibility = "visible";
+		}
+
+	} catch (e) {console.log(e.message);}
+
+	
+
+	
+});
+
+function capitalize(string){
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function buttonClick(){
+	self.port.emit("buttonClicked");
+}
+
+function changeBodySize(panelSize){
+	document.body.style.width = (panelSize.width - 2).toString() + "px";
+	document.body.style.height = (panelSize.height - 3).toString() + "px";
+
+}
---
+++ lib/ui/README.md
@@ -0,0 +1,31 @@
+
+Goals:
+
+- non-intrusive
+
+
+
+Questions:
+
+- prefilled value for answers?
+- pop when you pop, or wait?
+	- don't go while typing?  <- listen for key events?
+	- don't go while pageload?  <- 
+	- what else to wait to ignore?
+
+Button only appears when question is ready?  (can't do this)
+
+
+Pseudo Spec
+
+Interactions:
+- Panel / doorhanger / tab?
+- when it appears
+- what it says
+- how you know you voted
+- do you get to see your data?
+- 
+
+
+User Rejects this?  
+- Do we allow permanent opt-out?  If so, how?
